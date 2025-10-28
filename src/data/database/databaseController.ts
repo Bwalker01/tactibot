1fefda17786c84d684ec673b33082cab274c0679
Ben Walker <benjaminjwalker2910@gmail.com>
2025-10-28 16:57:39 +0000

    database implementation

---
+++ src/data/database/databaseController.ts
@@ -0,0 +1,35 @@
+import { Pool, QueryResultRow } from 'pg';
+
+/**
+ * A simple controller for the database.
+ * This class is responsible for querying the database and returning the results.
+ * It is also responsible for closing the connection pool when the application exits.
+ */
+export class DatabaseController {
+    private readonly pool: Pool;
+
+    constructor() {
+        this.pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+        });
+    }
+
+    public async queryAll<T extends QueryResultRow>(queryString: string, params?: any[]): Promise<{ rows: T[] }> {
+        const result = await this.pool.query<T>(queryString, params);
+        return { rows: result.rows };
+    }
+
+    public async queryOne<T extends QueryResultRow>(queryString: string, params?: any[]): Promise<T | null> {
+        const result = await this.pool.query<T>(queryString, params);
+        if (result.rows.length > 1) {
+            throw new Error('Multiple rows returned for a query expecting one.');
+        }
+        return result.rows[0] || null;
+    }
+
+    public async close() {
+        await this.pool.end();
+    }
+}
