5af53fcf24e661e5ebe21bd7d39342f16edef976
Ben Walker <benjaminjwalker2910@gmail.com>
2025-11-04 15:05:16 +0000

    run tests once

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,15 @@ module.exports = {
 	transform: {
 		...tsJestTransformCfg,
 	},
+	// Only run tests from src directory, exclude dist
+	testMatch: ['**/src/**/*.test.ts', '**/src/**/__test__/**/*.ts', '**/src/**/__tests__/**/*.ts'],
+	// Explicitly exclude dist and node_modules
+	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+	// Only collect coverage from src, not dist
+	collectCoverageFrom: [
+		'src/**/*.ts',
+		'!src/**/*.test.ts',
+		'!src/**/__test__/**',
+		'!src/**/__tests__/**',
+	],
 };
